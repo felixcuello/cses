49b278d168fe809b7c348e2bd00dc6a9b05fba7c
Felix <felix.cuello@gmail.com>
2025-12-23 07:42:05 -0300

    CPP modifications

--- missing_number.rb
+++
@@ -1,5 +0,0 @@
-n = readline.chomp.to_i
-n = n * (n + 1) / 2
-numbers = readline.chomp.split(' ').map(&:to_i)
-numbers.each { |i| n -= i }
-puts n
--- template.cc
+++ template.cc
@@ -1,12 +1,23 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <iomanip>
+#include <cstring>
 
+#define PI 3.14159265358979323846
 #define UL unsigned long
 #define ULL unsigned long long
 #define LL long long
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
         cin.sync_with_stdio(false);
         return 0;
 }
